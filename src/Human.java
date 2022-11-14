04c35f94dbf5508c9e1e0795f835ffad5646219a
Drfox81 <Drfox11181@mail.ru>
2022-11-14 15:56:13 +0300

    1

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,20 @@
 public class Human {
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String jobTitle;
+
+    public Human(String name, int yearOfBirth, String town, String jobTitle) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.town = town;
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town +
+                ". Я родился в " + yearOfBirth + "году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
 
 }
