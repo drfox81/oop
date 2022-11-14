7ab9149e929bbaaf20d05b7e207565580559108f
Drfox81 <Drfox11181@mail.ru>
2022-11-14 23:35:42 +0300

    готово

--- src/Human.java
+++ src/Human.java
@@ -1,20 +1,39 @@
+import java.util.Calendar;
+
 public class Human {
     public int yearOfBirth;
     public String name;
     public String town;
     public String jobTitle;
+    private int year = Calendar.getInstance().get(Calendar.YEAR);
 
     public Human(String name, int yearOfBirth, String town, String jobTitle) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.town = town;
-        this.jobTitle = jobTitle;
+        if (name == null || name == "") {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (yearOfBirth <= 0) {
+            this.yearOfBirth = year;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (town == null || town == "") {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (jobTitle == null || jobTitle == "") {
+            this.jobTitle = "Информация не указана";
+        } else {
+            this.jobTitle = jobTitle;
+        }
     }
 
     @Override
     public String toString() {
         return "Привет! Меня зовут " + name + ". Я из города " + town +
-                ". Я родился в " + yearOfBirth + "году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
+                ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human("Максим", 1988, "Минск","бренд-менеджер");
-        Human anna = new Human("Аня", 1995, "Москва","методист образовательных программ");
-        Human katy = new Human("Катя", 1992, "Калининград", "продакт-менеджер");
-        Human artem = new Human("Артем", 1995, "Москва", "дтректором по развитию бизнса");
+        Human maksim = new Human("Максим", -1988, "Минск","бренд-менеджер");
+        Human anna = new Human("", 1995, "Москва","методист образовательных программ");
+        Human katy = new Human("Катя", 1992, "", "продакт-менеджер");
+        Human artem = new Human("Артем", 1995, "Москва", "");
 
         System.out.println(maksim.toString());
         System.out.println(anna.toString());
